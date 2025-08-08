f7209358eae71510a63ea7361af8991d8751a8e0
david-vargas98 <edgar981125@live.com.mx>
2025-08-08 11:59:54 -0600

    feat: LINQ Queries.
    First queries to get into LINQ.

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -1,10 +1,37 @@
-﻿
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Linq;
+
 namespace Queries
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var context = new PlutoContext();
+
+            // LINQ syntax
+            Console.WriteLine("\tUsing LINQ Syntax:");
+            var coursesLINQ = from c in context.Courses
+                        where c.Name.Contains("C#")
+                        orderby c.Name
+                        select c;
+            // iterate through the results
+            foreach(var course in coursesLINQ)
+            {
+                Console.WriteLine(course.Name);
+            }
+
+            // Extension method syntax
+            Console.WriteLine("\tUsing Method Sintax:");
+            var coursesExt = context.Courses
+                .Where(c => c.Name.Contains("C#"))
+                .OrderBy(c => c.Name);
+            // iterate through the results
+            foreach(var course in coursesExt)
+            {
+                Console.WriteLine(course.Name);
+            }
         }
     }
 }
