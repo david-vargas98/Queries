ef56814dc0030f5dd54d4e42e63f9f17ad8bb5b4
david-vargas98 <edgar981125@live.com.mx>
2025-08-08 14:41:51 -0600

    feat: Type of Joins.
    Exercises with Inner join, which joins tables according to one or more condisitons.
    
    Exercises with Group join, it does not exists in SQL, but is used to make groups, is more likely to use aggregation functions.
    
    Finally, exercises with cross join, wich is the combination of the tables, it allows us to know all the possible combinations, in this case between two tables, Courses and Authors.

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -41,6 +41,46 @@ namespace Queries
                     Console.WriteLine($"{course.Name}");
                 }
             }
+            // Joining - Join: Joining courses with authors
+            var coursesJoinNavProp = from c in context.Courses // we don't need to use join, we can do so by using navigation properties
+                                select new                 // this is automaically translated to a join by EF
+                                {
+                                    CourseName = c.Name,
+                                    AuthorName = c.Author.Name
+                                };
+            var coursesJoin = from c in context.Courses // here we use explicit join
+                                   join a in context.Authors on c.Author.Id equals a.Id // they're joined by c.Author.Id with a.Id
+                                   select new
+                                   {
+                                       CourseName = c.Name,
+                                       AuthorName = a.Name
+                                   };
+            // Joining - Group Join: Grouping courses by their author
+            var coursesJoinGroup = from a in context.Authors
+                                   join c in context.Courses on a.Id equals c.AuthorId into g // into makes it a group join
+                                   select new
+                                   {
+                                       AuthorName = a.Name,
+                                       Courses = g.Count() // g is a collection of courses for each author and we count them
+                                   };
+            Console.WriteLine("\n\tCount of courses by Author");
+            foreach(var row in coursesJoinGroup)
+            {
+                Console.WriteLine($"Author: {row.AuthorName} | Courses: {row.Courses}");
+            }
+            // Joining - Cross Join: Cross join between authors and courses
+            var coursesCrossJoin = from a in context.Authors
+                                   from c in context.Courses // this is a cross join, every author with every course
+                                   select new
+                                   {
+                                       AuthorName = a.Name, // every author
+                                       CourseName = c.Name // every course
+                                   };
+            Console.WriteLine("\n\tCross Join Authors x Courses");
+            foreach(var row in coursesCrossJoin)
+            {
+                Console.WriteLine($"{row.AuthorName} - {row.CourseName}");
+            }
         }
     }
 }
