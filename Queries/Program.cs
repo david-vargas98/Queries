17ba5c3f03a93cd48479ce1b1ac50be5790b4b44
david-vargas98 <edgar981125@live.com.mx>
2025-08-08 16:04:44 -0600

    feat: Restriction and Ordering.
    Making some examples to know the Restriction and Ordering sintz for extension methods.

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using System.Diagnostics.Contracts;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 
 namespace Queries
 {
     class Program
     {
-        static void Main(string[] args)
+        static void LinqSintax(PlutoContext context)
         {
-            var context = new PlutoContext();
-
             // Restriction: Filtering data
             var coursesLevOne = from c in context.Courses
                                 where c.Level == 1 && c.Author.Id == 1 // restriction by level and author
@@ -30,10 +29,10 @@ namespace Queries
                               };
             // Grouping: Grouping courses by their level without using aggregation
             var coursesGrouped = from c in context.Courses
-                               group c by c.Level
+                                 group c by c.Level
                                into g
-                               select g;
-            foreach(var group in coursesGrouped)
+                                 select g;
+            foreach (var group in coursesGrouped)
             {
                 Console.WriteLine($"\tLevel: {group.Key} | Course count: {group.Count()}"); // as you see there's no aggregation
                 foreach (var course in group)
@@ -43,18 +42,18 @@ namespace Queries
             }
             // Joining - Join: Joining courses with authors
             var coursesJoinNavProp = from c in context.Courses // we don't need to use join, we can do so by using navigation properties
-                                select new                 // this is automaically translated to a join by EF
-                                {
-                                    CourseName = c.Name,
-                                    AuthorName = c.Author.Name
-                                };
+                                     select new                 // this is automaically translated to a join by EF
+                                     {
+                                         CourseName = c.Name,
+                                         AuthorName = c.Author.Name
+                                     };
             var coursesJoin = from c in context.Courses // here we use explicit join
-                                   join a in context.Authors on c.Author.Id equals a.Id // they're joined by c.Author.Id with a.Id
-                                   select new
-                                   {
-                                       CourseName = c.Name,
-                                       AuthorName = a.Name
-                                   };
+                              join a in context.Authors on c.Author.Id equals a.Id // they're joined by c.Author.Id with a.Id
+                              select new
+                              {
+                                  CourseName = c.Name,
+                                  AuthorName = a.Name
+                              };
             // Joining - Group Join: Grouping courses by their author
             var coursesJoinGroup = from a in context.Authors
                                    join c in context.Courses on a.Id equals c.AuthorId into g // into makes it a group join
@@ -64,7 +63,7 @@ namespace Queries
                                        Courses = g.Count() // g is a collection of courses for each author and we count them
                                    };
             Console.WriteLine("\n\tCount of courses by Author");
-            foreach(var row in coursesJoinGroup)
+            foreach (var row in coursesJoinGroup)
             {
                 Console.WriteLine($"Author: {row.AuthorName} | Courses: {row.Courses}");
             }
@@ -77,9 +76,47 @@ namespace Queries
                                        CourseName = c.Name // every course
                                    };
             Console.WriteLine("\n\tCross Join Authors x Courses");
-            foreach(var row in coursesCrossJoin)
+            foreach (var row in coursesCrossJoin)
             {
                 Console.WriteLine($"{row.AuthorName} - {row.CourseName}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            var context = new PlutoContext();
+            //LinqSintax(context);
+
+            // LINQ Extension Methods
+
+            // Restriction: Filtering data
+            var coursesLvlOne = context.Courses.Where(c => c.Level == 1); // filtering courses by level 1
+            Console.WriteLine("\tCourses filtered by Level 1");
+            foreach(var course in coursesLvlOne)
+            {
+                Console.WriteLine($"{course.Name}");
+            }
+
+            // Ordering: Sorting data by their level
+            var coursesLvlOneOrdered = context.Courses
+                .Where(c => c.Level == 1)
+                .OrderBy(c => c.Name); // filtering courses by level 1 and ordering them by name
+
+            Console.WriteLine("\n\tCourses ordered by Level and then by Full price");
+            var coursesOrdered = context.Courses
+                .OrderBy(c => c.Level)
+                .ThenBy(c => c.FullPrice); // ordering courses by level and then by full price
+            foreach (var course in coursesOrdered)
+            {
+                Console.WriteLine($"Lv.{course.Level} - {course.Name} - ${course.FullPrice}");
+            }
+
+            Console.WriteLine("\n\tCourses ordered by Level and then by Full price (Descending)");
+            var coursesOrderedDesc = context.Courses
+                .OrderBy(c => c.Level)
+                .ThenByDescending(c => c.FullPrice); // ordering courses by level and then by full price descending
+            foreach(var course in coursesOrderedDesc)
+            {
+                Console.WriteLine($"Lv.{course.Level} - {course.Name} - ${course.FullPrice}");
             }
         }
     }
