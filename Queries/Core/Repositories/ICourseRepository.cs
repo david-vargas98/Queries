4d6d032f808c0380de8667279074bece15a9e92f
david-vargas98 <edgar981125@live.com.mx>
2025-08-11 18:12:57 -0600

    refactor: Comments and usings.
    A comment was added and I've got rid of some using statements.

--- Queries/Core/Repositories/ICourseRepository.cs
+++ Queries/Core/Repositories/ICourseRepository.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Queries.Core.Repositories
 {
     public interface ICourseRepository : IRepository<Course>
     {
+        // Inhertis all methods from IRepository<Course> and adds two more specific methods for Course entity
         IEnumerable<Course> GetTopSellingCourses(int count);
         IEnumerable<Course> GetCoursesWithAuthors(int pageIndex, int pageSize);
     }
