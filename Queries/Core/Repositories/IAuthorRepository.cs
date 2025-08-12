f80097c1cf447c36a6889d21c5d1f10d4e02fa65
david-vargas98 <edgar981125@live.com.mx>
2025-08-11 18:30:09 -0600

    feat: IAuthorRepository and AuthorRepository.
    Creation and implementation of IAuthorRepository and AuthorRepository.

---
+++ Queries/Core/Repositories/IAuthorRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Queries.Core.Repositories
+{
+    public interface IAuthorRepository : IRepository<Author>
+    {
+        Author GetAuthorWithCourses(int id);
+    }
+}
---
+++ Queries/Persistence/Repositories/AuthorRepository.cs
@@ -0,0 +1,25 @@
+﻿using Queries.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+
+namespace Queries.Persistence.Repositories
+{
+    public class AuthorRepository : Repository<Author>, IAuthorRepository
+    {
+        public AuthorRepository(PlutoContext context) : base(context)
+        {
+        }
+
+        public PlutoContext PlutoContext
+        {
+            get { return Context as PlutoContext; }
+        }
+
+        public Author GetAuthorWithCourses(int id)
+        {
+            return PlutoContext.Authors.Include(a => a.Courses).SingleOrDefault(a => a.Id == id);
+        }
+    }
+}
